778100cefc56f3a78fc5af7b664413eea32fef81
Matt Boynes <mboynes@alleyinteractive.com>
2017-09-14 12:56:35 -0400

    initial commit

---
+++ php-71-test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Php 71 Test
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     php-71-test
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Php_71_Test
+ */
