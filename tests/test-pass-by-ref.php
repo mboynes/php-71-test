8eb9571f129e8f3a8f70f97d00e030007002e82d
Matt Boynes <mboynes@alleyinteractive.com>
2017-09-14 13:36:08 -0400

    Test ref_array functions

--- tests/test-pass-by-ref.php
+++ tests/test-pass-by-ref.php
@@ -18,6 +18,49 @@ class PassByRefTest extends WP_UnitTestCase {
 		$this->assertSame( 'changed', $arg );
 	}
 
+	function test_action_ref_array() {
+		add_action( 'testing-action', function( &$param1, $param2, &$param3, $param4 ) {
+			$param1 = true;
+			$param2 = true;
+			$param3 = true;
+			$param4 = true;
+		}, 10, 4 );
+
+		$arg1 = false;
+		$arg2 = false;
+		$arg3 = false;
+		$arg4 = false;
+
+		// Only args 1 and 3 should get changed here.
+		do_action_ref_array( 'testing-action', [ &$arg1, &$arg2, &$arg3, $arg4 ] );
+		$this->assertTrue( $arg1 );
+		$this->assertFalse( $arg2 );
+		$this->assertTrue( $arg3 );
+		$this->assertFalse( $arg4 );
+	}
+
+	function test_filter_ref_array() {
+		add_action( 'testing-filter', function( $param1, $param2, &$param3, $param4 ) {
+			$param1 = true;
+			$param2 = true;
+			$param3 = true;
+			$param4 = true;
+			return $param1;
+		}, 10, 4 );
+
+		$arg1 = false;
+		$arg2 = false;
+		$arg3 = false;
+		$arg4 = false;
+
+		// Only args 1 and 3 should get changed here.
+		$arg1 = apply_filters_ref_array( 'testing-filter', [ $arg1, &$arg2, &$arg3, $arg4 ] );
+		$this->assertTrue( $arg1 );
+		$this->assertFalse( $arg2 );
+		$this->assertTrue( $arg3 );
+		$this->assertFalse( $arg4 );
+	}
+
 	function test_pre_get_posts() {
 		$post_id = self::factory()->post->create();
 
